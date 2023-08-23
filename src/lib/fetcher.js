24000fabb552bb5fbcb184fa9456f778553bd287
athrur <you@example.com>
2023-08-23 23:35:03 +0100

    Change api url..

--- src/lib/fetcher.js
+++ src/lib/fetcher.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'https://ma-arthur-api-006042b68258.herokuapp.com/',
     timeout: 1000,
   });
   export default instance;
